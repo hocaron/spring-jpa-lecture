82a9183a9a2b05f822547fa00b8b204d7c053da8
haron47 <pooh0407@ewhain.net>
2022-01-03 17:46:32 +0900

    feat : 주문조회 플랫 데이터 최적화

--- src/main/java/jpabook/jpashop/repository/order/orderquery/OrderQueryDto.java
+++ src/main/java/jpabook/jpashop/repository/order/orderquery/OrderQueryDto.java
@@ -17,6 +17,7 @@ public class OrderQueryDto {
     private LocalDateTime orderDate; //주문시간
     private OrderStatus orderStatus;
     private Address address;
+
     private List<OrderItemQueryDto> orderItems;
 
     public OrderQueryDto(Long orderId, String name, LocalDateTime orderDate,
@@ -26,5 +27,14 @@ public class OrderQueryDto {
         this.orderDate = orderDate;
         this.orderStatus = orderStatus;
         this.address = address;
+    }
+
+    public OrderQueryDto(Long orderId, String name, LocalDateTime orderDate, OrderStatus orderStatus, Address address, List<OrderItemQueryDto> orderItems) {
+        this.orderId = orderId;
+        this.name = name;
+        this.orderDate = orderDate;
+        this.orderStatus = orderStatus;
+        this.address = address;
+        this.orderItems = orderItems;
     }
 }
